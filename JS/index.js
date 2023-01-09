bde8ce1bf8354cce867e26f29c2df7a6febe70a8
AndreyBal03 <74154813+AndreyBal03@users.noreply.github.com>
2023-01-09 12:24:58 -0600

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,11 @@
+
+
 document.addEventListener('DOMContentLoaded', function(){
    
     const btn = document.getElementById("init");
-    const title = document.getElementById("title")
 
     btn.onclick = function () {
-        title.style.opacity = "0%";
-        btn.style.opacity = '0%';
+	location.href="./scenes/load.html"
     }
 
 })
---
+++ JS/load.js
@@ -0,0 +1,5 @@
+const bgc = "rgb(94, 55, 25)";
+
+document.addEventListener('DOMContentLoaded', function(){
+	
+})
